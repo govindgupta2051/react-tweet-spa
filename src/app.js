019e0178989eb34bbc985165945fe06c9cfab5be
Govind <govind.g@turtlemint.com>
2018-03-25 16:29:02 +0530

    Basic setup of react-tweet-spa.

---
+++ src/app.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Greeting from './component/Gretting';
+
+import './styles/styles.scss';
+
+
+ReactDOM.render(<Greeting />, document.getElementById('app'));
